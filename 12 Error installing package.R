2fcd82869e171b3b0ce12091e2f85b4f94922a70
Saurabh Jain <28838017+sjain507@users.noreply.github.com>
2018-09-08 21:12:09 +0530

    created file 12

---
+++ 12 Error installing package.R
@@ -0,0 +1,6 @@
+# If you encounter this error: 
+'Warning in install.packages :
+  unable to access index for repository https://cran.rstudio.com/src/contrib:
+  cannot open URL 'https://cran.rstudio.com/src/contrib/PACKAGES''
+
+install.packages('package_name', dependencies=TRUE, repos='http://cran.rstudio.com/')
