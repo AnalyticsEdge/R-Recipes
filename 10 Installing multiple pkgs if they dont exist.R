678b35d8fc619957260861d5c44d86d78326783d
Saurabh Jain <28838017+AnalyticsEdge@users.noreply.github.com>
2018-09-30 12:29:35 +0530

    Edited file 10 to add the pkgs i use frequently

--- 10 Installing multiple pkgs if they dont exist.R
+++ 10 Installing multiple pkgs if they dont exist.R
@@ -1,12 +1,14 @@
-requiredPackages = c('caret','psych', 'shiny','shinydashboard')
+requiredPackages = c('caret', 'e1071',
+                     'tidyverse', 'lubridate','mice', 
+                     'prophet', 'zoo', 'forecast')
 for(p in requiredPackages)
 {
   if(!require(p,character.only = TRUE)) 
-    {
-      install.packages(p)
-      print(paste(p,"installed successfully"))
-      library(p,character.only = TRUE)
-    }
+  {
+    install.packages(p,dependencies=TRUE, repos='http://cran.rstudio.com/')
+    print(paste(p,"installed successfully"))
+    library(p,character.only = TRUE)
+  }
   else{print(paste(p , "library already installed"))}
 }
 print("List of the attached packages in your session: " )
